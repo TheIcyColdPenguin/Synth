13997fbbfe2612cdadc9f586bb3a2209b747043a
TheIcyColdPenguin <vinayakvineeth4@gmail.com>
2021-05-24 19:25:02 +0530

    add a dormant seek command

---
+++ src/commands/seek.ts
@@ -0,0 +1,41 @@
+import { OwnCommand, as, assertQueueConstruct, SongQueue, Song } from '../constants';
+import { playSong } from '../helpers';
+
+export default as<OwnCommand>({
+    name: 'seek',
+    aliases: ['mv', 'sk', 'move'],
+    description: 'seeks to a position in the song',
+    usage: 'seek time [absolute | a]',
+
+    cooldown: 3,
+    args: true,
+    voice: true,
+    guildOnly: true,
+
+    execute: (msg, args, queue) => {
+        if (!assertQueueConstruct(queue, msg)) {
+            return;
+        }
+
+        if (!queue.connection?.dispatcher) {
+            return void msg.channel.send('Unable to connect to voice channel');
+        }
+
+        const time = parseInt(args[0]);
+        const relativeOrAbsolute = args[1].toLowerCase() || 'relative';
+
+        if (!Number.isInteger(time)) {
+            return void msg.reply('Invalid timestamp, please enter a valid integer');
+        }
+
+        let absoluteTimeStamp = time;
+
+        if (relativeOrAbsolute === 'relative' || relativeOrAbsolute === 'r') {
+            absoluteTimeStamp += Math.floor(queue.connection.dispatcher.streamTime / 1000);
+        }
+
+        queue.connection.dispatcher.end();
+        queue.playing = false;
+        playSong(msg, queue, absoluteTimeStamp);
+    },
+});
