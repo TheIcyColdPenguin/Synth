05b5b5af652a5a444af14ab90340f73e12c64e16
TheIcyColdPenguin <vinayakvineeth4@gmail.com>
2022-02-26 09:03:42 +0530

    allow arbritrary backwards skipping

--- src/commands/previous.ts
+++ src/commands/previous.ts
@@ -1,5 +1,5 @@
 import { OwnCommand, as, assertQueueConstruct } from '../constants';
-import { playSong } from '../helpers';
+import { playSong, hasOnlyDigits, createEmbed } from '../helpers';
 
 export default as<OwnCommand>({
     name: 'previous',
@@ -20,11 +20,19 @@ export default as<OwnCommand>({
             return void msg.channel.send('Something went wrong talking to the voice channel');
         }
 
+        let num = hasOnlyDigits(args[0]);
+        if (!num) {
+            return void msg.channel.send(createEmbed('Argument must be greater than 1'));
+        }
+
         queue.connection.dispatcher?.end();
         queue.playing = false;
         queue.lastUsersListeningCheck = Date.now();
         if (queue.currSong !== 0) {
-            queue.currSong--;
+            queue.currSong -= num;
+            if (queue.currSong < 0) {
+                queue.currSong = 0;
+            }
         }
         playSong(msg, queue);
     },
